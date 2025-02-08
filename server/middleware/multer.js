1fed46e3be449c1905f21265e861a52535b06e03
KARTIK5799 <thekartiksapkal@gmail.com>
2025-02-08 13:44:51 +0530

    Backend:Refactor:added validation to middleware only xml files allowed

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,6 +1,14 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-const upload = multer({storage});
+const fileFilter = (req,file, cb)=>{
+    if(file.mimetype ==="text/xml" || file.mimetype === "application/xml"){
+        cb(null , true);
+    }else {
+        cb(new Error("Only XML file Allowed!"),false);
+    }
+}
+
+const upload = multer({storage,fileFilter});
 
 export default upload;
