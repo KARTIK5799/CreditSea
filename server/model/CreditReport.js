81c2586eb6b48e6d1f65f10593762fba907fab0e
KARTIK5799 <thekartiksapkal@gmail.com>
2025-02-08 12:12:10 +0530

    Backend: Added MongoDB collection named creditreport.

---
+++ server/model/CreditReport.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+
+const creditReportSchema = new mongoose.Schema({
+    name: String,
+    mobile: String,
+    pan: String,
+    creditScore: Number,
+    totalAccounts: Number,
+    activeAccounts: Number,
+    closedAccounts: Number,
+    currentBalance: Number,
+    securedAmount: Number,
+    unsecuredAmount: Number,
+    last7DaysEnquiries: Number,
+    creditCards: [String],
+    banks: [String],
+    addresses: [String],
+    accountNumbers: [String],
+    overdueAmount: Number,
+});
+
+export default mongoose("CreditReport", creditReportSchema)
